f454bfe547400cc2403c10abfc2de1803e899107
Ajay Rawat <104298002+AjayRawat0706@users.noreply.github.com>
2025-05-04 20:30:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,40 @@
 //your JS code here. If required.
+//your JS code here. If required.
+
+
+
+let fontSize=document.getElementById("fontsize")
+
+let color=document.getElementById("fontcolor")
+
+let save=document.querySelector("input[type='submit']")
+window.addEventListener("DOMContentLoaded",()=>{
+
+	  const cookies = document.cookie.split('; ');
+
+	cookies.forEach(cookie => {
+        const [name, value] = cookie.split('=');
+		  if (name === 'fontsize') {
+            document.body.style.fontSize = value + 'px';
+			  fontSize.value = value;
+        } else if (name === 'fontcolor') {
+            document.body.style.color = value;
+			  color.value = value;
+        }
+    });
+    save.addEventListener(("click"),(e)=>{
+
+e.preventDefault();
+		
+const fontSizeValue=fontSize.value
+const colorValue=color.value
+
+	   document.cookie = `fontsize=${fontSizeValue}; path=/;`;
+        document.cookie = `fontcolor=${colorValue}; path=/;`;
+
+
+		  document.body.style.fontSize = fontSizeValue + 'px';
+        document.body.style.color = colorValue;
+    })
+
+})
